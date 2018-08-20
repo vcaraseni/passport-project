3d4fbb533132964c639832aeeff117d28c0f30b6
vcaraseni <vcaraseni@gmail.com>
2018-08-20 13:03:36 +0300

    passport added

--- config/app.php
+++ config/app.php
@@ -147,6 +147,9 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        // Passport
+        Laravel\Passport\PassportServiceProvider::class,
+
         /*
          * Package Service Providers...
          */
